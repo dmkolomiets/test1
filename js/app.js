b0b354ba2272e266390462758f8813754dda0e5d
Dmitry <dm.kolomiets@gmail.com>
2022-11-06 17:36:56 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const answer = [];
+
+answer[0] = prompt('Your first name?', '');
+answer[1] = prompt('Your Last name?', '');
+answer[2] = prompt('How ald are you?', '');
+
+document.write(answer[0], answer[1], answer[2]);
